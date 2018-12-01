3388804bb0700861560fccf9c5456c581d4dc664
punee <puneet.malik@gmail.com>
2018-12-01 19:45:44 +0530

    with code

---
+++ backend/models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create new instance of the mongoose.schema. the schema takes an
+// object that shows the shape of your database entries.
+const CommentsSchema = new Schema({
+  author: String,
+  text: String,
+}, { timestamps: true });
+
+// export our module to use in server.js
+//export default mongoose.model('Comment', CommentsSchema);
+module.exports = mongoose.model('Comment',CommentsSchema);
